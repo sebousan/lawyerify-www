ac88a405df6cd71e690e6cb01831baf69cd6573d
sebousan <sebousan@gmail.com>
2022-11-15 15:41:35 +0100

    content

---
+++ static2/admin/app/collections.js
@@ -0,0 +1,16 @@
+// Types
+import casestudies from './content/casestudies.js';
+import pages from './content/pages.js';
+import indexes from './content/indexes.js';
+
+// Data
+import config from './data/config.js';
+
+export const collections = [
+    pages,
+    indexes,
+    casestudies,
+    config
+]
+
+export default collections
