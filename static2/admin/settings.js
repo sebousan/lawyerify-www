215cd94bc9d0d3f119459f5e61e32d1597230295
sebousan <sebousan@gmail.com>
2022-11-16 16:51:42 +0100

    add admin settings

---
+++ static2/admin/settings.js
@@ -0,0 +1,33 @@
+export const availableCollections = [
+    // 'posts',
+    // 'categories',
+    // 'authors',
+    // 'projects',
+    // 'types',
+    'casestudies',
+    // 'publications'
+];
+
+export const availableTypeSection = [
+    // { label: 'Actualité', value: 'posts' },
+    { label: 'Cas d’études', value: 'casestudies' },
+    // { label: 'Projet', value: 'projects' },
+    // { label: 'Parution', value: 'publications' }
+]
+
+export const availableBlocks = [
+    'cta',
+    'map',
+    'blockquote',
+    'latest',
+    'editorial',
+    'embed',
+    'figure',
+    'infos',
+    'paragraph',
+    'selectedprojects',
+    'gauges',
+    'title'
+];
+
+export default availableCollections;
